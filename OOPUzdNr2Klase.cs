0d522639b5d6684eece315e6e193707a41227ee0
Kestus0756 <58221968+Kestus0756@users.noreply.github.com>
2024-01-03 19:45:59 +0200

    v1
    v1

---
+++ OOPUzdNr2Klase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UzdNr2 {
+    public class SekosSkaiciavimas // Sekos skaiciavimo klase
+    {
+        public SekosSkaiciavimas(int n)
+        {
+            Skaiciavimas(n);
+            Example.Datetime();
+        }
+
+        static void Skaiciavimas(int n)
+        {
+            long pirmasSk = 1;
+            long antrasSk = 2;
+            for (int i = 2; i < n; i++)
+            {
+                long sk = pirmasSk * antrasSk;
+                if (sk > 1000000000000000)
+                {
+                    Console.WriteLine("Eil. nr. atsakymo: " + i);
+                    Console.WriteLine("Galutinis atsakymas: " + sk);
+                }
+
+                pirmasSk = antrasSk;
+                antrasSk = sk;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+
+namespace UzdNr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 11;
+            SekosSkaiciavimas sekosSkaiciavimas = new SekosSkaiciavimas(n);
+        }
+    }
+
+    public class Example
+    { // Nr. 2 klase is duotos svetaines
+        public static void Datetime()
+        {
+            DateTime start = DateTime.Now;
+            System.Threading.Thread.Sleep(5000);
+            DateTime end = DateTime.Now;
+
+            TimeSpan ts = (end - start);
+            Console.WriteLine("Elapsed Time is {0} ms", ts.TotalMilliseconds);
+        }
+    }
+}
